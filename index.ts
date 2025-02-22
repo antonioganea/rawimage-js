9f9541b27e718418261013bbd7926ab5b9e65e95
Antonio Ganea <antonio.ganea@yahoo.com>
2025-02-22 16:04:35 +0200

    Simplified code

--- index.ts
+++ index.ts
@@ -18,3 +18,18 @@ app.get("/", (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+
+/*
+
+To obtain the WASM & js,
+
+Compile this file : https://github.com/ncruces/dcraw/blob/master/dcraw.c
+
+With emcc, with the following settings :
+
+emcc dcraw.c -o dcraw.js -sMODULARIZE=1 -sEXPORTED_FUNCTIONS=_main -sEXIT_RUNTIME=0 -sEXPORTED_RUNTIME_METHODS=FS,callMain
+-sTOTAL_MEMORY=1GB -sMAXIMUM_MEMORY=2GB -sALLOW_MEMORY_GROWTH=1 -sSTACK_SIZE=512MB -sINITIAL_HEAP=256MB -sALLOW_TABLE_GROWTH=1 -O3 -lm -DNODEPS
+
+(Memory settings can be tweaked to your needs)
+
+*/
